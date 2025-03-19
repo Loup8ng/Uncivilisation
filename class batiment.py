58c686e8f28c26dac1281f0caeb4d07a30dc8e64
AryZack <aryzack01@gmail.com>
2025-03-19 10:54:17 +0100

    Class batiment

---
+++ class batiment.py
@@ -0,0 +1,24 @@
+from pygame import *
+import cvs
+
+class batiment():
+    def __init__(self,nom,cout,temps_construction,capacite_stockage=None,prerequis=None):
+        """Permet d'initialiser les batiments, en donnant :le nom du batiment, 
+        son coût en ressources, son temps de construction, sa capacité de stockage 
+        si il en as une, et des prérequis pour pouvoir le construire si il y en a."""
+        self.nom= nom
+        self.cout= cout
+        self.temps_construction = temps_construction
+        self.capacite_stockage = capacite_stockage if capacite_stockage != 0
+        self.prerequis = prerequis if prerequis != []
+
+        def afficher_infos(self):
+            """Afficher l'ensemble des informations du batiments"""
+             infos = "Bâtiment: " + (self.nom)\n
+             "Coût: " + (self.cout)\n
+             "Temps de construction: " + (self.temps_construction) + " tours"\n
+        if self.capacite_stockage:
+            infos += "Capacité de stockage: " + (self.capacite_stockage)\n
+        if self.prerequis:
+            infos += "Prérequis: " + (self.prerequis)\n
+        return infos
