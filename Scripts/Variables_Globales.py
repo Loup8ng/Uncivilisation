0f07275ec95b8955b9b1dda9364e7e30d14d4e38
creusator <elias.morin@hotmail.com>
2025-04-18 20:13:59 +0200

    Equilibrage poids génération du monde

--- Scripts/Variables_Globales.py
+++ Scripts/Variables_Globales.py
@@ -11,9 +11,8 @@ COLOR_RED = (255, 0, 0)
 
 biomes_disponibles = ["montagne", "plaine", "foret", "sable", "lac"]
 biome_weights = {
-    "plaine":     {"plaine": 40, "foret": 50, "sable": 10},
-    "foret":      {"foret": 30, "plaine": 10, "montagne": 60},
-    "montagne":   {"montagne": 40, "foret": 30, "plaine": 30},
-    "sable":      {"sable": 30, "plaine": 15, "lac": 55},
-    "lac":        {"lac": 70, "sable": 25, "plaine": 5}
-}
+    "plaine":     {"plaine": 35, "foret": 50, "sable": 15},
+    "foret":      {"plaine": 20,"foret": 20, "montagne": 60},
+    "montagne":   {"plaine": 30, "foret": 30, "montagne": 40 },
+    "sable":      {"plaine": 20, "sable": 30, "lac": 50},
+    "lac":        {"plaine": 10, "sable": 40, "lac": 50}}
